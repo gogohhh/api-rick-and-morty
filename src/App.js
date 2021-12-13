24324c324473fe4f8c36533900d7990dc38ee81d
gogohhh <gogo_hhh_d@hotmail.com>
2021-12-13 11:43:44 -0600

    Conexion a paginacion

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import Header from './components/Header.jsx';
 import Characters from './components/Characters.jsx';
+import Paginacion from './components/Paginacion.jsx';
 
 
 function App() {
 
   const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
 
   const url = 'https://rickandmortyapi.com/api/character'
 
   const fetchCharacters = (url) => {
     fetch(url)
       .then(response => response.json())
-      .then(data => setCharacters(data.results))
+      .then((data) => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
       .catch(error => console.error('Error: ', error))
   };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
 
   useEffect(() => {
     fetchCharacters(url);
@@ -23,8 +36,9 @@ function App() {
   return (
     <div>
       <Header brand="Rick and Morty App"/>
-
+      <Paginacion prev={info.prev} next={info.prev} onPrevious={onPrevious} onNext={onNext} />
       <Characters characters={characters}/>
+
     </div>
   );
 }
---
+++ src/components/Paginacion.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function Paginacion({prev, next, onPrevious, onNext}) {
+
+    const handlePrevius = () => {
+        onPrevious();
+    }
+
+    const handleNext = () => {
+        onNext();
+    }
+
+    return(
+        <nav>
+            <ul className="pagination justify-content-center mt-5" >
+                { prev ? (
+                        <li className="page-item">
+                            <button className="page-link" onClick={ handlePrevius }>Anteior</button>
+                        </li>
+                    ) : null
+                }
+
+                { next ? (
+                <li className="page-item">
+                    <button className="page-link" onClick={ handleNext }>Siguiente</button>
+                </li>
+                ) : null
+                }
+            </ul>
+        </nav>
+    );
+}
