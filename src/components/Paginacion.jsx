603514cbf6e28b13d85990ba5042a3f7a9d6055c
gogohhh <gogo_hhh_d@hotmail.com>
2021-12-13 11:47:37 -0600

    Paginacion sin condicionar botones

--- src/components/Paginacion.jsx
+++ src/components/Paginacion.jsx
@@ -13,19 +13,15 @@ export default function Paginacion({prev, next, onPrevious, onNext}) {
     return(
         <nav>
             <ul className="pagination justify-content-center mt-5" >
-                { prev ? (
-                        <li className="page-item">
-                            <button className="page-link" onClick={ handlePrevius }>Anteior</button>
-                        </li>
-                    ) : null
-                }
+               
+                <li className="page-item">
+                    <button className="page-link" onClick={ handlePrevius }>Anteior</button>
+                </li>
 
-                { next ? (
+                
                 <li className="page-item">
                     <button className="page-link" onClick={ handleNext }>Siguiente</button>
                 </li>
-                ) : null
-                }
             </ul>
         </nav>
     );
